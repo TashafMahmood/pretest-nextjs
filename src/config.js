46b4b7f4dfa7b80723460b3c96eb86da467a5b13
TashafMahmood <tashaf@elred.io>
2024-09-04 09:55:00 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -2,4 +2,13 @@ export const baseURL = process.env.API_URL;
 export const webviewURL = process.env.REACT_WEBVIEW_URL;
 export const appHeaderKey1 = process.env.APP_HEADER_KEY_1;
 export const appHeaderKey2 = process.env.APP_HEADER_KEY_2;
-console.log("develop branch")
+console.log(
+  baseURL,
+  "baseURL",
+  webviewURL,
+  "webviewURL",
+  appHeaderKey1,
+  "appHeaderKey1",
+  appHeaderKey2,
+  "appHeaderKey2"
+);
