659b891af1e62b823ad46212df3cd56e52705300
TashafMahmood <tashaf@elred.io>
2025-06-27 10:11:21 +0530

    changes

--- src/component/LogoutPayment/LogoutPayment.jsx
+++ src/component/LogoutPayment/LogoutPayment.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./index.module.css";
 import { useLogout } from "@/context/LogoutContext";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -21,6 +21,18 @@ const LogoutPayment = () => {
     router.push(nccode ? `/membership?nccode=${nccode}` : "/membership");
     setIsOpen(false);
   };
+
+
+useEffect(() => {
+  // Prevent scroll on body when modal is open
+  const originalOverflow = document.body.style.overflow;
+  document.body.style.overflow = "hidden";
+
+  return () => {
+    // Restore scroll when modal is closed
+    document.body.style.overflow = originalOverflow;
+  };
+}, []);
 
   return (
     <div className={style.container_div}>
