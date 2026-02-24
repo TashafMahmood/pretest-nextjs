3779785c81aac915c9071b2c2f748b8445a38033
TashafMahmood <tashaf@elred.io>
2026-02-24 13:41:34 +0530

    changes the card height

--- src/pages/event-details/index.js
+++ src/pages/event-details/index.js
@@ -348,8 +348,8 @@ const EventDetailsPage = ({ eventData, eventId }) => {
         />
 
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
